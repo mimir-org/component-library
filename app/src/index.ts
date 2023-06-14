83f5b8c06a7077728d42d2aa104399e67d552dd0
Reidar Liabø <reidar.liabo@gmail.com>
2023-06-14 11:42:57 +0200

    Removed reference to system color

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,13 +1,12 @@
 /// <reference types="./theme" />
 /// <reference types="./global" />
-export type { SystemColors } from "./components/core/theme/variables/color/color";
 export * from "./components/atoms";
 export * from "./components/core/hooks";
 export * from "./components/core/props";
 export * from "./components/core/services";
 export * from "./components/core/theme";
+export * from "./components/icons";
 export * from "./components/layouts";
 export * from "./components/molecules";
 export * from "./components/organism";
 export * from "./components/types";
-export * from "./components/icons";
