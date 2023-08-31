05e3e747f5f7e29674d6ab4a9dec248747771527
Andreas Eikefet Kristensen <andreas.kristensen@bouvet.no>
2023-08-31 13:37:14 +0200

    Added show password functionallity

--- app/src/components/molecules/inputs/input/Input.tsx
+++ app/src/components/molecules/inputs/input/Input.tsx
@@ -1,8 +1,9 @@
-import { ForwardedRef, forwardRef, InputHTMLAttributes, isValidElement, ReactElement } from "react";
+import { ForwardedRef, forwardRef, InputHTMLAttributes, isValidElement, ReactElement, useState } from "react";
 import { Icon } from "../../../atoms";
 import { Sizing } from "../../../core/props";
 import { Box } from "../../../layouts";
-import { InputContainer, InputIconContainer } from "./Input.styled";
+import { IconContainer, InputContainer, InputIconContainer } from "./Input.styled";
+import { Eye, EyeSlash } from "@styled-icons/heroicons-outline";
 
 export type InputProps = InputHTMLAttributes<HTMLInputElement> &
   Omit<Sizing, "boxSizing"> & {
@@ -17,6 +18,11 @@ export const Input = forwardRef((props: InputProps, ref: ForwardedRef<HTMLInputE
   const { width, maxWidth, minWidth, height, maxHeight, minHeight, icon, iconPlacement, type, ...delegated } = props;
   const isHidden = type === "hidden";
 
+  const [showPassword, setShowPassword] = useState(false);
+  const currentShowPassword = showPassword ? "text" : "password";
+  const isPasswordField = type === "password" ? true : false;
+  const showPasswordIcon = showPassword ? <EyeSlash height="24px" width="24px" /> : <Eye height="24px" width="24px" />;
+
   return (
     <Box
       display={isHidden ? "none" : undefined}
@@ -28,18 +34,22 @@ export const Input = forwardRef((props: InputProps, ref: ForwardedRef<HTMLInputE
       maxWidth={maxWidth}
       minWidth={minWidth}
     >
-      <InputContainer ref={ref} type={type} iconPlacement={iconPlacement} icon={icon} {...delegated} />
-      {icon && (
-        <InputIconContainer iconPlacement={iconPlacement} icon={icon}>
-          {isValidElement(icon) ? icon : <Icon src={String(icon)} alt="" />}
-        </InputIconContainer>
+      <InputContainer
+        ref={ref}
+        type={isPasswordField ? currentShowPassword : type}
+        iconPlacement={iconPlacement}
+        icon={icon}
+        {...delegated}
+      />
+      {isPasswordField ? (
+        <IconContainer onClick={() => setShowPassword(!showPassword)}>{showPasswordIcon}</IconContainer>
+      ) : (
+        icon && (
+          <InputIconContainer iconPlacement={iconPlacement} icon={icon} type={currentShowPassword}>
+            {isValidElement(icon) ? icon : <Icon src={String(icon)} alt="" />}
+          </InputIconContainer>
+        )
       )}
     </Box>
   );
 });
-
-Input.displayName = "Input";
-Input.defaultProps = {
-  height: "40px",
-  iconPlacement: "right",
-};
